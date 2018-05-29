f44f5886c7af7074ff9201193bfa152a503250ff
Murat Demirci <mdemirci@outlook.com>
2018-05-29 15:10:23 +0300

    frontend works

---
+++ SimpleCommerce/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCommerce.Data;
+
+namespace SimpleCommerce.Controllers
+{
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(ApplicationDbContext context):base(context)
+        {
+
+        }
+        public IActionResult Index(int categoryId)
+        {
+            return View();
+        }
+    }
+}
